d31b074f70635efb1ff0bd7eb1ec98691028fe83
Leo Ruiz <ruizruizleo@hotmail.com>
2022-09-26 22:20:22 -0300

    Objetos com construtor e logica dentro do metodo

---
+++ Aula 08/index.js
@@ -0,0 +1,17 @@
+const { Conta } = require("./funcoes");
+
+const novaConta = new Conta(12345,0001,"123.123.123-12",100);
+
+console.log(novaConta.saldo);
+
+novaConta.depositar(250);
+
+console.log(novaConta.saldo);
+
+console.log(novaConta.sacar(100));
+
+const message = novaConta.sacar(400);
+
+console.log(message);
+
+console.log(novaConta.saldo);
