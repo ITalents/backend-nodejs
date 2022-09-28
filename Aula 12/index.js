1905f1c88bc3eabf58c9dddbc97902005d9b4ff5
Leo Ruiz <ruizruizleo@hotmail.com>
2022-09-28 19:35:27 -0300

    Manipulando JSON

---
+++ Aula 12/index.js
@@ -0,0 +1,17 @@
+const jsonEntrada = '{"nome":"Leonardo","idade": 25,"professor": true,"turmas":[10,12,34,42],"endereco":{ "rua":"alameda teste", "numero":123,"bairro":"centro","CEP":"123.123-12"}}'
+
+const obj = JSON.parse(jsonEntrada);
+
+console.log(obj.turmas);
+
+obj.turmas.map( function(valor, posicao) {
+    console.log(`posição: ${posicao}, valor: ${valor}`);
+});
+
+obj.nome += " Orabona";
+obj.salario = 10000;
+
+console.log(obj);
+
+const jsonString = JSON.stringify(obj);
+console.log(jsonString);
