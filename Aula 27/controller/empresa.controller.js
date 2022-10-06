960c9b8ff2e0d96f46d2a8c1a2bb8a53b3166d18
Leo Ruiz <ruizruizleo@hotmail.com>
2022-10-06 13:34:57 -0300

    try catch

---
+++ Aula 27/controller/empresa.controller.js
@@ -0,0 +1,90 @@
+const Empresa = require("../model/empresa");
+const mongoose = require("mongoose");
+
+const find = async (req,res) => {
+    try{
+        const id = mongoose.Types.ObjectId(req.params.id);
+        let found = false;
+
+        const empresa = await Empresa.findById(id);
+
+        if(empresa != null){
+            found = true;
+        }
+
+        if(!found){
+            return res.status(404).send({message: "Empresa nao foi encontrada, tente outro ID"});
+        }
+
+        return res.status(200).send(empresa);
+
+    }catch(err){
+        console.log(`erro: ${err}`);
+        return res.status(500).send("erro no servidor, tente novamente mais tarde");
+    }
+}
+
+const findAllEmpresas = async (req,res) => {
+    return res.status(200).send(await Empresa.find());
+}
+
+const createEmpresa = async (req,res) => {
+    const empresa = req.body;
+    
+    if(Object.keys(empresa).length === 0){
+        return res.status(400).send({message: "O corpo da mensagem esta vazio"});
+    }
+
+    if(!empresa.nome){
+        return res.status(400).send({message: "O campo 'nome' nao foi encontrado!"})
+    }
+
+    if(!empresa.numFuncionarios){
+        return res.status(400).send({message: "O campo 'numFuncionarios' nao foi encontrado!"})
+    }
+
+    return res.status(201).send(await Empresa.create(empresa));
+}
+
+const updateEmpresa = async (req, res) => {
+    const id = req.params.id;
+    const empresa = req.body;
+    //let found = false;
+
+    if(Object.keys(empresa).length === 0){
+        return res.status(400).send({message: "O corpo da mensagem esta vazio"});
+    }
+
+    if(!empresa.nome){
+        return res.status(400).send({message: "O campo 'nome' nao foi encontrado!"})
+    }
+
+    if(!empresa.numFuncionarios){
+        return res.status(400).send({message: "O campo 'numFuncionarios' nao foi encontrado!"})
+    }
+
+    return res.status(200).send(await Empresa.findByIdAndUpdate(id,empresa, {returnDocument: "after"}));
+    // if(!found){
+    //     res.status(404).send({message: "Nao foi encontrado"});
+    // }
+}
+
+const deleteEmpresa = async (req, res) => {
+    const id = req.params.id;
+    //let found = false;
+
+    return res.status(200).send(await Empresa.findByIdAndRemove(id));
+
+    // if(!found){
+    //     res.status(404).send({message: "Nao foi encontrado"});
+    // } 
+}
+
+
+module.exports = {
+    find,
+    findAllEmpresas,
+    createEmpresa,
+    updateEmpresa,
+    deleteEmpresa
+}
