0124391ba4dea900d741fc48936fccd84351ab78
Leo Ruiz <ruizruizleo@hotmail.com>
2022-10-06 18:58:54 -0300

    autenticacao

---
+++ Aula 29/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const connectToDatabase = require("./database/database");
+const authService = require("./service/auth.service");
+const app = express();
+
+connectToDatabase();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("hello world");
+});
+
+app.post("/login", async  (req, res) => {
+    try{
+        const { email, senha } = req.body;
+        const user = await authService.loginService(email);
+
+        if(!user){
+        return res.status(400).send({ message: "Usuario nao encontrado, tente novamente"});
+        }
+
+        if(senha != user.senha){
+            return res.status(400).send({ message: "Senha invalida"});
+        }
+
+        res.send(user);
+    }catch(err){
+        console.log(`erro: ${err}`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
